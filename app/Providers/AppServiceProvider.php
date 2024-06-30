af5c753fcbf0279fb95da2348cccc0f4c90d81ff
Godwin Uche <godwin.uche@paynovate.com>
2024-06-30 08:08:06 +0100

    UPDATE: Added RateLimiting middleware for the API

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(RateLimiter $rateLimiter): void
     {
-        //
+        $rateLimiter->for('api', fn () => Limit::perMinute(20));
     }
 }
